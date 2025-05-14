51d4b04e17fb56144054d1102fa59030bc9d136c
Maxence Nho <78363268+PiKwaii93@users.noreply.github.com>
2025-05-14 16:42:08 +0200

    feat(auth): add try catch

--- app/controllers/auth_controller.ts
+++ app/controllers/auth_controller.ts
@@ -1,25 +1,44 @@
 import { HttpContext } from '@adonisjs/core/http'
 import AuthService from '#services/auth_service'
+import { ErrorHandlerService } from '#services/error_handler_service'
 
 export default class AuthController {
-  async login({ request, auth }: HttpContext) {
-    const { email, password } = request.only(['email', 'password'])
-    const service = new AuthService(auth.use('jwt'))
-    return await service.login(email, password)
+  private readonly errorHandler: ErrorHandlerService
+
+  constructor() {
+    this.errorHandler = new ErrorHandlerService()
   }
 
-  async register({ request, auth }: HttpContext) {
-    const { email, password, roleId } = request.only(['email', 'password', 'roleId'])
-    const service = new AuthService(auth.use('jwt'))
-    return await service.register(email, password, roleId)
-  }
-
-  async me({ auth }: HttpContext) {
-    if (!(await auth.check())) {
-      throw new Error('Unauthorized')
+  async login({ request, auth, response }: HttpContext) {
+    try {
+      const { email, password } = request.only(['email', 'password'])
+      const service = new AuthService(auth.use('jwt'))
+      return await service.login(email, password)
+    } catch (error) {
+      this.errorHandler.handle(error, { request, response }, 'login')
     }
+  }
 
-    const service = new AuthService(auth.use('jwt'))
-    return await service.me()
+  async register({ request, auth, response }: HttpContext) {
+    try {
+      const { email, password, roleId } = request.only(['email', 'password', 'roleId'])
+      const service = new AuthService(auth.use('jwt'))
+      return await service.register(email, password, roleId)
+    } catch (error) {
+      this.errorHandler.handle(error, { request, response }, 'register')
+    }
+  }
+
+  async me({ auth, request, response }: HttpContext) {
+    try {
+      if (!(await auth.check())) {
+        throw new Error('Unauthorized')
+      }
+
+      const service = new AuthService(auth.use('jwt'))
+      return await service.me()
+    } catch (error) {
+      this.errorHandler.handle(error, { request, response }, 'fetch user data')
+    }
   }
 }
